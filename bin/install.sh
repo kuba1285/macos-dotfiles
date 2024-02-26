60941dcdde15bfb6d88289401ff58473894b9eb1
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-27 08:14:13 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -111,7 +111,7 @@ if [[ $YN = y ]] ; then
     cp $PARENT/src/* /Users/$USER/bin/
     echo -e "$COK - Installed."
 
-    echo export \""PATH="\$PATH:/Users/$USER/bin\" >> ~/.zshrc
+    echo -e export "PATH=\"\$PATH:/Users/$USER/bin\"\nneofetch" >> ~/.zshrc
     echo -e "TMOUT=900\nTRAPALRM() { tput bold && tput setaf 2 && gcc /Users/$USER/bin/donut.c -o /Users/$USER/bin/donut && /Users/$USER/bin/donut }" >> ~/.zshrc
 fi
 
