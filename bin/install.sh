818ea72b16c79fbba832d2c2b2dea99df805a285
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-27 08:05:35 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -112,7 +112,6 @@ if [[ $YN = y ]] ; then
     echo -e "$COK - Installed."
 
     echo export \""PATH="\$PATH:/Users/$USER/bin\" >> ~/.zshrc
-    echo "neowofetch --gap -30 --ascii \"\$(fortune -s | pokemonsay -w 30)\""  >> ~/.zshrc
     echo -e "TMOUT=900\nTRAPALRM() { tput bold && tput setaf 2 && gcc /Users/$USER/bin/donut.c -o /Users/$USER/bin/donut && /Users/$USER/bin/donut }" >> ~/.zshrc
 fi
 
