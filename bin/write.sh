0f93ff08d4e5956e88d964460a579d3436736379
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-27 08:44:34 +0900

    Update write.sh

--- bin/write.sh
+++ bin/write.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat << EOF >> ~/.zshrc
-export PATH="$PATH:/Users/$USER/bin"
+export PATH="\$PATH:/Users/$USER/bin"
 neofetch
 TMOUT=900
 TRAPALRM() { tput bold && tput setaf 2 && gcc /Users/$USER/bin/donut.c -o /Users/$USER/bin/donut && /Users/$USER/bin/donut }
