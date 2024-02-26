20af66ae972e55b334e239c8f976f6172fab6b99
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-27 08:46:30 +0900

    Update write.sh

--- bin/write.sh
+++ bin/write.sh
@@ -6,3 +6,6 @@ neofetch
 TMOUT=900
 TRAPALRM() { tput bold && tput setaf 2 && gcc /Users/$USER/bin/donut.c -o /Users/$USER/bin/donut && /Users/$USER/bin/donut }
 EOF
+
+# yabai sudoers setting
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
