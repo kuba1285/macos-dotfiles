bf74804b3a34f43826ff2277a20179b156c311ae
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-27 14:15:05 +0900

    Update write.sh

--- bin/write.sh
+++ bin/write.sh
@@ -9,3 +9,27 @@ EOF
 
 # yabai sudoers setting
 echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
+
+cat << EOF >> ~/.zshrc
+# Generate a new XML property list
+XMLLIST="\$(plutil -create xml1 - )"
+PLIST="/Users/$USER/Library/Application Support/com.apple.wallpaper/Store/Index.plist"
+
+# Inserts the 'SystemDefault' key into the newly created binary property list
+# and retrieves and sets the value from the XML property list. 
+# Then overwrite and save to the wallpaper settings file.
+plutil -create binary1 - | plutil -insert 'SystemDefault' -xml "\${XMLLIST}" -o "\${PLIST}" -
+killall WallpaperAgent
+
+# pywal setting
+WAL_IMGS=(\$(ls -d /Users/$USER/Pictures/*))
+SEC=\`date +%S\`
+I=\$((SEC%\$(echo \${#WAL_IMGS[@]})+1))
+osascript -e "
+tell application \\"System Events\\"
+	tell every desktop
+		set picture to \\"\${WAL_IMGS[\$I]}\\"
+	end tell
+end tell"
+wal -qni \${WAL_IMGS[\$I]}
+EOF
