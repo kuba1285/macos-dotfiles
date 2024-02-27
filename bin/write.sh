625c138de472e34a8f4a104b8d5c80e4f4aac871
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-27 15:19:07 +0900

    Update write.sh

--- bin/write.sh
+++ bin/write.sh
@@ -4,7 +4,7 @@ cat << EOF >> ~/.zshrc
 export PATH="\$PATH:/Users/$USER/bin"
 neofetch
 TMOUT=900
-TRAPALRM() { tput bold && tput setaf 2 && gcc /Users/$USER/bin/donut.c -o /Users/$USER/bin/donut && /Users/$USER/bin/donut }
+TRAPALRM() { tput bold && tput setaf 3 && gcc /Users/$USER/bin/donut.c -o /Users/$USER/bin/donut && /Users/$USER/bin/donut }
 EOF
 
 # yabai sudoers setting
