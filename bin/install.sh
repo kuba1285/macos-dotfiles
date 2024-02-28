1dabeadb7d194a68b514197898e370c78d220292
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-28 15:58:04 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -107,6 +107,7 @@ if [[ $YN = y ]] ; then
     tar xvzf v1.4.0.tar.gz
     cd 3d-ascii-viewer*
     make
+    find ./models -name "*.mtl" -type f | xargs rm
     mv 3d-ascii-viewer models /Users/$USER/bin/.
     echo -e "$COK - Installed."
 fi
