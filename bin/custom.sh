65b5571f697c5960d21ea9b10f0fd167966cee0a
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-28 16:05:48 +0900

    Create custom.sh

---
+++ bin/custom.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd
+git clone http://github.com/possatti/pokemonsay &>> $INSTLOG
+cd pokemonsay
+./install.sh &>> $INSTLOG
+
+pipx install pywal
+pipx ensurepath
+
+cd
+wget https://github.com/autopawn/3d-ascii-viewer/archive/refs/tags/v1.4.0.tar.gz
+tar xvzf v1.4.0.tar.gz
+cd 3d-ascii-viewer*
+make
+find ./models -name "*.mtl" -type f | xargs rm
+mv 3d-ascii-viewer models /Users/$USER/bin/.
