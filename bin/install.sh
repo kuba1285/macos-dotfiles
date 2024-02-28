7751ee55449fc210e406e739b3d21bce9ea4e61d
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-28 16:06:32 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -94,21 +94,7 @@ fi
 # Install custom app
 wait_yn $'[\e[1;33mACTION\e[0m] - Would you like to install custom app?'
 if [[ $YN = y ]] ; then
-    cd
-    git clone http://github.com/possatti/pokemonsay &>> $INSTLOG
-    cd pokemonsay
-    ./install.sh &>> $INSTLOG
-
-    pipx install pywal
-    pipx ensurepath
-
-    cd
-    wget https://github.com/autopawn/3d-ascii-viewer/archive/refs/tags/v1.4.0.tar.gz
-    tar xvzf v1.4.0.tar.gz
-    cd 3d-ascii-viewer*
-    make
-    find ./models -name "*.mtl" -type f | xargs rm
-    mv 3d-ascii-viewer models /Users/$USER/bin/.
+    source $BIN/custom.sh
     echo -e "$COK - Installed."
 fi
 
