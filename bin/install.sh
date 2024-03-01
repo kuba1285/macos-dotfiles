6b192117be93906519942a14a11c8c118c72a1ea
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-02 06:39:25 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -46,7 +46,7 @@ show_progress() {
         echo -n "."
         sleep 2
     done
-    echo -en "Done!\n"
+    echo "Done!\n"
     sleep 2
 }
 
@@ -64,34 +64,34 @@ set_colors
 # give the user an option to exit out
 wait_yn "${YELLOW}ACITION${RESET} - Would you like to start with the install?"
 if [[ $YN = y ]] ; then
-    echo -e "${CYAN}NOTE${RESET} - Setup starting..."
+    echo "${CYAN}NOTE${RESET} - Setup starting..."
 else
-    echo -e "${CYAN}NOTE${RESET} - This script will now exit, no changes were made to your system."
+    echo "${CYAN}NOTE${RESET} - This script will now exit, no changes were made to your system."
     exit
 fi
 
 # Install CLI for Xcode
-echo -en "${CYAN}NOTE${RESET} - Now installing CLI for Xcode."
+echo "${CYAN}NOTE${RESET} - Now installing CLI for Xcode."
 xcode-select --install &>> $INSTLOG
 show_progress $!
-echo -e "${GREEN}OK${RESET} - Installed."
+echo "${GREEN}OK${RESET} - Installed."
 
 # Install rosetta
 wait_yn "${YELLOW}ACITION${RESET} - Would you like to install rosetta?"
 if [[ $YN = y ]] ; then
     sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license &>> $INSTLOG
     show_progress $!
-    echo -e "${GREEN}OK${RESET} - Installed."
+    echo "${GREEN}OK${RESET} - Installed."
 fi
 
 # Install homebrew
 if ! type brew &> /dev/null ; then
-    echo -en "${CYAN}NOTE${RESET} - Now installing Homebrew."
+    echo "${CYAN}NOTE${RESET} - Now installing Homebrew."
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &>> $INSTLOG
     show_progress $!
-    echo -e "${GREEN}OK${RESET} - Installed."
+    echo "${GREEN}OK${RESET} - Installed."
 else
-    echo -e "${CYAN}NOTE${RESET} - Since Homebrew is already installed, skip this phase and proceed."
+    echo "${CYAN}NOTE${RESET} - Since Homebrew is already installed, skip this phase and proceed."
 fi
 
 # Homebrew path setting
@@ -101,31 +101,31 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 wait_yn "${YELLOW}ACITION${RESET} - Would you like to install app from Brewfile?"
 if [[ $YN = y ]] ; then
     brew bundle install --file $BIN/Brewfile &>> $INSTLOG
-    echo -e "${GREEN}OK${RESET} - Installed."
+    echo "${GREEN}OK${RESET} - Installed."
 fi
 
 # Install custom app
 wait_yn "${YELLOW}ACITION${RESET} - Would you like to install custom app?"
 if [[ $YN = y ]] ; then
     source $BIN/custom.sh
-    echo -e "${GREEN}OK${RESET} - Installed."
+    echo "${GREEN}OK${RESET} - Installed."
 fi
 
 # Copy Config Files
 wait_yn "${YELLOW}ACITION${RESET} - Would you like to copy config files?"
 if [[ $YN = y ]] ; then
-    echo -e "${CYAN}NOTE${RESET} - Copying config files..."
+    echo "${CYAN}NOTE${RESET} - Copying config files..."
 
     # copy the configs directory
     cp -rT $PARENT/. ~/ &>> $INSTLOG
     cp $PARENT/src/* /Users/$USER/bin/
-    echo -e "${GREEN}OK${RESET} - Installed."
+    echo "${GREEN}OK${RESET} - Installed."
 fi
 
 # A bootplug to match the binary format so that yabai can inject code into the Dock of arm64 binaries.
 if [[ $(uname -m) == 'arm64' ]]; then
     sudo nvram boot-args=-arm64e_preview_abi
-    echo -en "${GREEN}OK${RESET} - A bootplug to match the binary format so that yabai can inject code into the Dock of arm64 binaries."
+    echo "${GREEN}OK${RESET} - A bootplug to match the binary format so that yabai can inject code into the Dock of arm64 binaries."
 fi
 
 # Write settings
@@ -144,4 +144,4 @@ yabai --start-service
 skhd --start-service
 
 # Script is done
-echo -e "${CYAN}NOTE${RESET} - Script had completed!"
+echo "${CYAN}NOTE${RESET} - Script had completed!"
