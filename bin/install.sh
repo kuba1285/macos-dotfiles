f46ceb7cc8f874cfadb8672049a4650ff918d871
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-02 06:33:36 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -15,18 +15,30 @@
 # 4. Restart your computer.
 # 5. Restart your computer.
 
-# set some colors
-CNT="[\e[1;36mNOTE\e[0m]"
-COK="[\e[1;32mOK\e[0m]"
-CER="[\e[1;31mERROR\e[0m]"
-CAT="[\e[1;37mATTENTION\e[0m]"
-CWR="[\e[1;35mWARNING\e[0m]"
-
 # Define variables
 BIN=$(cd $(dirname $0); pwd)
 PARENT=$(cd $(dirname $0)/../; pwd)
 INSTLOG="$BIN/install.log"
 ######
+
+# set some colors
+set_colors() {
+    if [ -t 1 ]; then
+        RED=$(tput setaf 1)
+        GREEN=$(tput setaf 2)
+        YELLOW=$(tput setaf 3)
+        CYAN=$(tput setaf 6)
+        BOLD=$(tput bold)
+        RESET=$(tput sgr0)
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        CYAN=""
+        BOLD=""
+        RESET=""
+    fi
+}
 
 # function that would show a progress bar to the user
 show_progress() {
@@ -47,72 +59,73 @@ wait_yn(){
 ######
 
 clear
+set_colors
 
 # give the user an option to exit out
-wait_yn $'[\e[1;33mACTION\e[0m] - Would you like to start with the install?'
+wait_yn "${YELLOW}ACITION${RESET} - Would you like to start with the install?"
 if [[ $YN = y ]] ; then
-    echo -e "$CNT - Setup starting..."
+    echo -e "${CYAN}NOTE${RESET} - Setup starting..."
 else
-    echo -e "$CNT - This script will now exit, no changes were made to your system."
+    echo -e "${CYAN}NOTE${RESET} - This script will now exit, no changes were made to your system."
     exit
 fi
 
 # Install CLI for Xcode
-echo -en "$CNT - Now installing CLI for Xcode."
+echo -en "${CYAN}NOTE${RESET} - Now installing CLI for Xcode."
 xcode-select --install &>> $INSTLOG
 show_progress $!
-echo -e "$COK - Installed."
+echo -e "${GREEN}OK${RESET} - Installed."
 
 # Install rosetta
-wait_yn $'[\e[1;33mACTION\e[0m] - Would you like to install rosetta?'
+wait_yn "${YELLOW}ACITION${RESET} - Would you like to install rosetta?"
 if [[ $YN = y ]] ; then
     sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license &>> $INSTLOG
     show_progress $!
-    echo -e "$COK - Installed."
+    echo -e "${GREEN}OK${RESET} - Installed."
 fi
 
 # Install homebrew
 if ! type brew &> /dev/null ; then
-    echo -en "$CNT - Now installing Homebrew."
+    echo -en "${CYAN}NOTE${RESET} - Now installing Homebrew."
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &>> $INSTLOG
     show_progress $!
-    echo -e "$COK - Installed."
+    echo -e "${GREEN}OK${RESET} - Installed."
 else
-    echo -e "$CNT - Since Homebrew is already installed, skip this phase and proceed."
+    echo -e "${CYAN}NOTE${RESET} - Since Homebrew is already installed, skip this phase and proceed."
 fi
 
 # Homebrew path setting
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 
 # Install app from Brewfile
-wait_yn $'[\e[1;33mACTION\e[0m] - Would you like to install app from Brewfile?'
+wait_yn "${YELLOW}ACITION${RESET} - Would you like to install app from Brewfile?"
 if [[ $YN = y ]] ; then
     brew bundle install --file $BIN/Brewfile &>> $INSTLOG
-    echo -e "$COK - Installed."
+    echo -e "${GREEN}OK${RESET} - Installed."
 fi
 
 # Install custom app
-wait_yn $'[\e[1;33mACTION\e[0m] - Would you like to install custom app?'
+wait_yn "${YELLOW}ACITION${RESET} - Would you like to install custom app?"
 if [[ $YN = y ]] ; then
     source $BIN/custom.sh
-    echo -e "$COK - Installed."
+    echo -e "${GREEN}OK${RESET} - Installed."
 fi
 
 # Copy Config Files
-wait_yn $'[\e[1;33mACTION\e[0m] - Would you like to copy config files?'
+wait_yn "${YELLOW}ACITION${RESET} - Would you like to copy config files?"
 if [[ $YN = y ]] ; then
-    echo -e "$CNT - Copying config files..."
+    echo -e "${CYAN}NOTE${RESET} - Copying config files..."
 
     # copy the configs directory
     cp -rT $PARENT/. ~/ &>> $INSTLOG
     cp $PARENT/src/* /Users/$USER/bin/
-    echo -e "$COK - Installed."
+    echo -e "${GREEN}OK${RESET} - Installed."
 fi
 
 # A bootplug to match the binary format so that yabai can inject code into the Dock of arm64 binaries.
 if [[ $(uname -m) == 'arm64' ]]; then
     sudo nvram boot-args=-arm64e_preview_abi
-    echo -en "$COK - A bootplug to match the binary format so that yabai can inject code into the Dock of arm64 binaries."
+    echo -en "${GREEN}OK${RESET} - A bootplug to match the binary format so that yabai can inject code into the Dock of arm64 binaries."
 fi
 
 # Write settings
@@ -131,4 +144,4 @@ yabai --start-service
 skhd --start-service
 
 # Script is done
-echo -e "$CNT - Script had completed!"
+echo -e "${CYAN}NOTE${RESET} - Script had completed!"
